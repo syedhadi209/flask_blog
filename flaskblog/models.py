d5cd53e9589ae47d5b413fab830f0120fb9706a5
syedhadi209 <hadi.alvi@devsinc.com>
2023-06-16 18:26:04 +0500

    crud funtionality added

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -2,31 +2,33 @@ from datetime import datetime
 from flaskblog import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 
-class User(db.Model,UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    img_file = db.Column(db.String(20), nullable=False, default="defalt.jpg")
+    img_file = db.Column(db.String(20), nullable=False, default="default.jpeg")
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True,primaryjoin="User.id == Post.user_id")
+    posts = db.relationship('Post', backref='author',
+                            lazy=True, primaryjoin="User.id == Post.user_id")
     # posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User({self.username}, {self.email} , {self.password})"
-    
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100) ,nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=True,
+                            default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post({self.title})"
